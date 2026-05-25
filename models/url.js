b871353d104f1c57c41db878748879bd84594ee6
Yashwanth V <vyashwanth179@gmail.com>
2026-05-25 19:30:41 +0530

    Link URLs to Users in the Database

--- models/url.js
+++ models/url.js
@@ -22,6 +22,11 @@ const urlSchema= new mongoose.Schema({
     date:{
         type:Date,
         default: Date.now,
+    },
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:false,
     }
 });
 
